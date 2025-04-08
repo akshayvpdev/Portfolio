9e62753e2ff28d89b7fa40c1005f7ba5b0c5889d
akshayvpdev <akshayvpofficial@gmail.com>
2025-04-08 14:38:09 +0530

    new commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = nextConfig;
+const nextConfig = {
+    eslint: {
+      ignoreDuringBuilds: true,
+      // Only run ESLint on specific directories
+      dirs: ['pages', 'components']
+    }
+  };
+  
+  module.exports = nextConfig;
